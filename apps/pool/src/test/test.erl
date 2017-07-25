de9dd86522d6e843050e6af00840b5bc788cd315
六爷 <MySixGod@users.noreply.github.com>
2017-07-25 21:10:01 +0800

    1.add

---
+++ apps/pool/src/test/test.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author cwt  <woshi6ye@gmail.com>
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. 七月 2017 9:41
+%%%-------------------------------------------------------------------
+-module(test).
+-author("cwt").
+
+%% API
+-export([]).
+
+-compile(export_all).
+
+test1() ->
+  progress_pool:execute(test, say, []).
+
+t2() ->
+  t:start_link(),
+  gen_server:call(t,{}, 1000).
+
+
+say() ->
+  hello.
+
+
+ex() ->
+  case catch throw(error) of
+    Message ->
+      io:format("Message:~p~n",[Message])
+  end.
---
+++ deps/test/ebin/test.app
@@ -0,0 +1,8 @@
+{application,test,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {mod,{test,[]}},
+              {env,[]},
+              {modules,[gen,gen_test,test]}]}.
